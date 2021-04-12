a68f02387b804eef4b6d37e5776fe889ae7fef19
Spartanzer <73786137+Spartanzer@users.noreply.github.com>
2021-04-12 23:31:28 +0200

    Update exo.c
    Rajout du début de code de Mauro.

--- exo.c
+++ exo.c
@@ -1,3 +1,18 @@
+int biggestOfThree(int a, int b, int c)
+{
+	a = a > b ? a : b;
+	return a > c ? a : c;
+}
+int biggestOfFour(int a, int b, int c, int d)
+{
+	a = biggestOfThree(a, b, c);
+	return a > d ? a : d;
+}
+int biggestOfFive(int a, int b, int c, int d, int e)
+{
+	a = biggestOfFour(a, b, c, d);
+	return a > e ? a: e;
+}
 int min(int a, int b)
 {
     return a < b ? a : b;
