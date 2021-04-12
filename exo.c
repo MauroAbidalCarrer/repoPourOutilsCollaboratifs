60ee7d8d340730e4cf090fbb0a3cc712480e4759
Omar ANOUAR <omaranouar300@gmail.com>
2021-04-12 22:59:49 +0200

    Rajout de bout de code en C.

--- exo.c
+++ exo.c
@@ -1,9 +1,17 @@
-int biggestOfThree(int a, int b, int c)
+int min(int a, int b)
 {
-a = a > b ? a : b;
-return a > c ? a : c;
+    return a < b ? a : b;
 }
-int biggesOfFour(int a, int b, int c, int d)
+int secondBiggsetOfThree(int a, int b, int c)
 {
-
+    a = a > b ? a : c;
+    return min(a, b);
+}
+int secondBiggestOfFour(int a, int b, int c, int d)
+{
+    return min(biggestOfThree(a, b, c), d);
+}
+int secondBiggestOfFive(int a, int b, int c, int d, int e)
+{
+    return min(biggestOfFour(a, b, c, d), e);
 }
